83d29559aad2dd32ffda42396c296621e1c35f65
Awa Danfa <31821970+AwaDanfa@users.noreply.github.com>
2023-02-20 18:01:32 +0000

    Added initial files

---
+++ app.js
@@ -0,0 +1,12 @@
+// Add functionality to the switch/toggler button to toggle between light and dark mode using javascript
+
+/* function darkMode() {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+}
+ */
+
+let element = document.body;
+const darkMode = () => {
+  element.classList.toggle('dark-mode');
+}
